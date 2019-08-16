8bad78471a643e9add40f825e34a4a00be80dc14
yyuvly <apple4916@naver.com>
2019-08-16 10:19:04 +0900

    customer, finance 매핑

---
+++ src/net/customer/action/FaqBoardAction.java
@@ -0,0 +1,16 @@
+package net.customer.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.common.action.Action;
+import net.common.action.ActionForward;
+
+public class FaqBoardAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		return null;
+	}
+
+}
--- src/net/finance/controller/FinanceController.java
+++ src/net/finance/controller/FinanceController.java
@@ -1,41 +1,66 @@
 package net.finance.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class FinanceController
- */
+import net.common.action.Action;
+import net.common.action.ActionForward;
+import net.finance.action.TokenExchangeListAction;
+import net.finance.action.TokenWithdrawalListAction;
+
+
 @WebServlet("/FinanceController")
-public class FinanceController extends HttpServlet {
+public class FinanceController extends HttpServlet implements Servlet{
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public FinanceController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8"); 
+		String RequestURI = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String command = RequestURI.substring(contextPath.length());
+		ActionForward forward = null;
+		Action action = null;
+		
+		try {
+			switch (command) {
+			case "TokenExchangeList.fn":
+				action = new TokenExchangeListAction();
+				forward = action.execute(request, response);
+				break;
+			case "TokenWithdrawalList.fn":
+				action = new TokenWithdrawalListAction();
+				forward = action.execute(request, response);
+				break;
+			default:
+				break;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		if (forward != null) {
+			if (forward.isRedirect()) {
+				response.sendRedirect(forward.getPath());
+			} else {
+				RequestDispatcher dispatcher = request.getRequestDispatcher(forward.getPath());
+				dispatcher.forward(request, response);
+			}
+		}
+	}
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doProcess(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		doProcess(request, response);
 	}
-
 }
