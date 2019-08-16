57d888ac2864c191e7a19a1acb610ece5c8a9720
younsik-kim <53412963+younsik-kim@users.noreply.github.com>
2019-08-16 10:21:16 +0900

    member, setting 매핑 끝~~

---
+++ src/net/member/action/MemberListAction.java
@@ -0,0 +1,20 @@
+package net.member.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.common.action.Action;
+import net.common.action.ActionForward;
+
+public class MemberListAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		request.setCharacterEncoding("utf-8"); //�ѱ�ó��
+		ActionForward forward = new ActionForward();
+		forward.setRedirect(false);
+		forward.setPath("./member/memberList.jsp");
+		return forward;
+	}
+
+}
