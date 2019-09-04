ab83adf23a4504b588fed1603c8656a25bed96ee
OvollovO <40098076+psk876@users.noreply.github.com>
2019-09-04 17:52:14 +0900

    gregre
    gregre

--- src/net/member/dao/MemberDAO.java
+++ src/net/member/dao/MemberDAO.java
@@ -8,8 +8,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-//import com.mysql.jdbc.CallableStatement;
-import com.mysql.cj.jdbc.CallableStatement;
+import com.mysql.jdbc.CallableStatement;
+//import com.mysql.cj.jdbc.CallableStatement;
 
 import net.customer.dto.inquiryAnswer;
 import net.member.dto.Member;
