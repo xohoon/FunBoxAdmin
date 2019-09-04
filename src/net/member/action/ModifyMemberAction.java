ee9e40f6e8f0f36118f712bb3357d8d80a3ee5de
younsik-kim <53412963+younsik-kim@users.noreply.github.com>
2019-09-04 17:44:39 +0900

    ModifyCompanyAction 작성

--- src/net/member/action/ModifyMemberAction.java
+++ src/net/member/action/ModifyMemberAction.java
@@ -2,13 +2,11 @@ package net.member.action;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 
 import org.json.simple.JSONObject;
 
 import net.common.action.Action;
 import net.common.action.ActionForward;
-import net.company.dto.CompanyDetail;
 import net.json.MemberJson;
 import net.member.dto.Member;
 
@@ -16,95 +14,49 @@ public class ModifyMemberAction implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		ActionForward forward = new ActionForward();
-		CompanyDetail companyDetail = new CompanyDetail();
-		int cp_idx = 13;
-		String cp_sector = request.getParameter("cp_sector");
-		System.out.println("test : " + cp_sector);
-		String cp_name;
-		String cp_branch;
-		String cp_manager;
-		String cp_number;
-		String cp_capital;
-		String cp_address;
-		String cp_monthly_profit;
-		String iv_contraction_during;
-		String iv_min_amount;
-		String iv_appl_stock;
-		String iv_goal_amount;
-		String iv_appl_day;
-		String cf_thumbnail;
-		//String[] cf_store_images;
-		String cf_pr_background;
-		String cp_point_title;
-		String cp_point_content;
-		//String[] cf_business_plan_images;
-		String pl_year;
-		String pl_month;
-		String calA;
-		String calB;
-		String calC;
-		String calD;
-		String calE;
-		String calT;
-		String cal1;
-		String cal2;
-		String cal3;
-		String cal4;
-		String cal5;
-		String cal6;
-		String cal7;
-		String tot1;
-		String tot2;
-		String tot3;
-		String tot4;
-		String tot5;
-		
-		
-		// return null;
-		/*
-		if (result == 0) {
-			response.setContentType("text/html;charset=utf-8");
-			PrintWriter out = response.getWriter();
-			out.println("<script>");
-			out.println("alert('기업신청에 실패했습니다.\n다시 시도해주세요.');");
-			out.println("location.href='./companycompanyDetailForm.cp';");
-			out.println("</script>");
-			out.close();
-			return null;
-		}else if(result == 1) {
-			response.setContentType("text/html;charset=utf-8");
-			PrintWriter out = response.getWriter();
-			out.println("<script>");
-			out.println("alert('기업신청이 완료되었습니다.');");
-			out.println("location.href='./index.jsp';");
-			out.println("</script>");
-			out.close();
-		}*/
-		return null;	
-	}
-	
-	// filenName get
-	private String extractFileName(Part part) {
-		String contentDisp = part.getHeader("content-disposition");
-		String[] items = contentDisp.split(";");
-		for (String s : items) {
-			if (s.trim().startsWith("filename")) {
-				return s.substring(s.indexOf("=") + 2, s.length() - 1);
-			}
-		}
-		return "NULL";
-	}
+		MemberJson memberJson = new MemberJson();
+		Member member = new Member();
 
-	private String getFileFormat(Part part) {
-		String contentDisp = part.getHeader("content-disposition");
-		String[] items = contentDisp.split(";");
-		for (String s : items) {
-			if (s.trim().startsWith("filename")) {
-				return "." + s.substring(s.lastIndexOf(".") + 1,s.length()-1);
-			}
+		String mb_idx_string = request.getParameter("mb_idx");
+		Integer mb_idx = 0;
+		
+		try {
+			mb_idx = Integer.parseInt(mb_idx_string);
+		} catch (NumberFormatException nfe) {
+			//Exception processing
 		}
-		return "";
+		
+		String mb_name = request.getParameter("mb_name");
+		String mb_id = request.getParameter("mb_id");
+		String mb_email = request.getParameter("mb_email");
+		String mb_phone = request.getParameter("mb_phone");
+		String mb_add_num = request.getParameter("mb_add_num");
+		String mb_add_ch = request.getParameter("mb_add_ch");
+		String mb_add_more = request.getParameter("mb_add_more");
+		String mb_add_extra = request.getParameter("mb_add_extra");
+		String mb_recommend = request.getParameter("mb_recommend");
+		String mb_point = request.getParameter("mb_point");
+		String mb_token = request.getParameter("mb_token");
+		
+		member.setMb_idx(mb_idx);
+		
+		member.setMb_name(mb_name);
+		member.setMb_id(mb_id);
+		member.setMb_email(mb_email);
+		member.setMb_phone(mb_phone);
+		member.setMb_add_num(mb_add_num);
+		member.setMb_add_ch(mb_add_ch);
+		member.setMb_add_more(mb_add_more);
+		member.setMb_add_extra(mb_add_extra);
+		member.setMb_recommend(mb_recommend);
+		member.setMb_point(mb_point);
+		member.setMb_token(mb_token);
+
+		JSONObject jsonObject = memberJson.modifyMemberResult(member);
+		response.setContentType("application/x-json; charset=UTF-8");
+		response.getWriter().print(jsonObject);
+
+		return null;
 	}
 
 }
